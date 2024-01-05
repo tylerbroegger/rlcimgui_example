42bcc3256cc625c59a521acc1b0a277268023853
Tyler Brögger <71833469+tylerbroegger@users.noreply.github.com>
2024-01-05 01:49:15 +0100

    main.c

--- main.c
+++ main.c
@@ -1,51 +1,46 @@
-/*******************************************************************************************
-*
-*   raylib game template
-*
-*   <Game title>
-*   <Game description>
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "include/raylib.h"
 #define  CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "include/cimgui.h"
 #include "include/rlImGui.h"
 
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
+ #define GRUV_BG (Color){ 40, 40, 40, 255 }
+
 int main(void)
 {
-    // Initialization
-    //---------------------------------------------------------
-    InitWindow(1200, 800, "ImGui In C");
-    SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
-
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(1280, 720, "ImGui In C");
+    SetTargetFPS(60);
     rlImGuiSetup(true);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(GRAY);
-
+        ClearBackground(GRUV_BG);
         rlImGuiBegin();
 
         igShowDemoWindow(NULL);
-        rlImGuiEnd();
 
+		if (igBeginMainMenuBar()) {
+			if (igBeginMenu("File", true)) {
+				if (igMenuItem_Bool("Open", "Ctrl+O", false, true)) {}
+				if (igMenuItem_Bool("Save", "Ctrl+S", false, true)) {}
+				if (igMenuItem_Bool("Close", "Ctrl+Q", false, true)) {}
+				igEndMenu();
+			}
+			if (igBeginMenu("Edit", true)) {
+				if (igMenuItem_Bool("Open", "Ctrl+O", false, true)) {}
+				if (igMenuItem_Bool("Save", "Ctrl+S", false, true)) {}
+				if (igMenuItem_Bool("Close", "Ctrl+Q", false, true)) {}
+				igEndMenu();
+			}
+			igEndMainMenuBar();
+		}
+
+
+        rlImGuiEnd();
         EndDrawing();
     }
     rlImGuiShutdown();
-
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
